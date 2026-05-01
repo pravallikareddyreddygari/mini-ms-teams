82deecb5c7af53ac2164e90429002295b8ad75df
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-05-01 13:06:02 +0530

    favicon fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Teams Clone",
   description: "Mini Microsoft Teams clone",
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='75'>👥</text></svg>",
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👥</text></svg>",
   },
 };
 
